b79581e77e23ddf3d690659282393a6f11b78269
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2026-04-09 08:51:02 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 // client/vite.config.ts
 import { defineConfig, type ProxyOptions } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
